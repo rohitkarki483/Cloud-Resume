986f61ad6b6b36a0f884e4b7cf8d95bd483b8ca6
Rohit Karki <rohitkarki8848@gmail.com>
2026-01-07 22:44:46 +0545

    rohit

--- output.tf
+++
@@ -1,4 +0,0 @@
-output "api_url" {
-  description = "Base URL of the API Gateway"
-  value       = aws_apigatewayv2_api.resume_api.api_endpoint
-}
--- variables.tf
+++
@@ -1,5 +0,0 @@
-variable "bucket_name" {
-  description = "S3 bucket name for resume website"
-  type        = string
-  default     = "rohit-cloud-resume-terraform-tf"
-}
